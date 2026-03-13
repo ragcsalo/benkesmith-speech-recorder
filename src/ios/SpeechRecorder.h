513ed70352d6ea2bb3416242fabac3ec6c4998e0
Bence Kovacs <kov.benc@gmail.com>
2026-03-13 17:38:06 +0100

    Update SpeechRecorder.h

--- src/ios/SpeechRecorder.h
+++ src/ios/SpeechRecorder.h
@@ -1,6 +1,14 @@
 #import <Cordova/CDV.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface GetMediaPlugin : CDVPlugin
-- (void)getLast:(CDVInvokedUrlCommand*)command;
+@interface SpeechRecorder : CDVPlugin <AVAudioRecorderDelegate>
+
+@property (nonatomic, strong) AVAudioRecorder *recorder;
+@property (nonatomic, strong) NSString *callbackId;
+@property (nonatomic, strong) NSTimer *timer;
+
+- (void)voicerec_audio_start:(CDVInvokedUrlCommand*)command;
+- (void)voicerec_audio_stop:(CDVInvokedUrlCommand*)command;
+- (void)voicerec_audio_restart:(CDVInvokedUrlCommand*)command;
+
 @end
-
